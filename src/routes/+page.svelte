24255ae4ccb1a031653b013ae7ab826ef035e29d
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-06-28 20:01:23 +0100

    Update homepage with new styling and motivational message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to <span class=""> liber notarum </span></h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mt-5">
+	<h1 class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Welcome to <span class="capitalize"> liber notarum </span> 🖊
+	</h1>
+	<h4 class="scroll-m-20 text-center text-xl font-semibold tracking-tight">Get ready pumped! 💪</h4>
+</div>
