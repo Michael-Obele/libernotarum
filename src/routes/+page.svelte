9e06a00dd9303085c86b6a6d3a4bfa4bd3d6a63e
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-07-17 04:27:22 +0100

    🎨 Improve structure / format of the code.
    Changes:
    - path for a background color to the layout component in +layout.svelte in +page.svelte.
    - Updated the text and background colors in the various sections of +page.svelte.
     favicon files: about.txt, android-chrome-192x192.png, android-chrome-512x512.png, apple-touch-icon.pngx16.png, favicon-32x32.png, favicon.ico, and site to include new color and text styles.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,49 +6,49 @@
 	<title>Liber Notarum</title>
 </svelte:head>
 
-<div class="mx-auto mb-5 mt-10 w-fit">
+<div class="mx-auto w-fit pb-5 pt-10">
 	<Logo />
 </div>
 
-<header class="bg-blue-500 p-6 text-white">
+<header class="bg-primary-light text-secondary-light p-6">
 	<h1 class="text-4xl">
 		Liber Notarum
-		<span class="font-mono text-gray-800">
-			[<span class="font-bold">ˈliː.ber noˈtaː.rum</span>]
+		<span class="text-accent1-light font-mono">
+			\<span class="font-bold">ˈliːbər noʊˈtɑːrəm</span>\
 		</span>
 	</h1>
 	<p class="text-lg">Note-taking, reimagined.</p>
 </header>
 
-<main class="p-6">
+<main class="bg-gray-900 p-6 dark:bg-gray-600">
 	<section class="my-6">
-		<p class="font-mono text-2xl text-gray-800">
-			[<span class="font-bold">ˈliː.ber noˈtaː.rum</span>]
+		<p class="font-mono text-2xl text-red-600 dark:text-red-600">
+			\<span class="font-bold">ˈliːbər noʊˈtɑːrəm</span>\
 		</p>
-		<p class="text-gray-700">
+		<p class="text-red-600 dark:text-red-600">
 			The Latin term "Liber Notarum" translates to "Book of Notes," representing a digital notebook
 			for capturing, organizing, and sharing knowledge.
 		</p>
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">
+		<h2 class="text-2xl font-bold text-red-600 dark:text-red-600">
 			Open-source digital notebook for modern note-taking.
 		</h2>
-		<p class="text-gray-700">
+		<p class="text-red-600 dark:text-red-600">
 			A constantly evolving project providing powerful note-taking capabilities with an intuitive
 			interface, available completely free and without attribution.
 		</p>
-		<a
+		<!-- <a
 			href="/signup"
-			class="mt-4 inline-block rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+			class="mt-4 inline-block rounded bg-red-600 px-4 py-2 text-white hover:bg-red-700 dark:bg-red-400 dark:hover:bg-red-500"
 			>Sign up now</a
-		>
+		> -->
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">synopsis</h2>
-		<p class="text-gray-700">
+		<h2 class="text-2xl font-bold text-gray-100 dark:text-gray-200">synopsis</h2>
+		<p class="text-gray-300 dark:text-gray-700">
 			Capture, organize, and share your thoughts, ideas, and knowledge with ease. Liber Notarum
 			offers a clean, intuitive interface, robust security, and seamless collaboration. Customize
 			your notes with Markdown support, templates, and task management. Access your notes from any
@@ -57,16 +57,16 @@
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">related words</h2>
-		<p class="text-gray-700">
+		<h2 class="text-2xl font-bold text-gray-100 dark:text-gray-200">related words</h2>
+		<p class="text-gray-300 dark:text-gray-700">
 			Note-taking, Organization, Collaboration, Productivity, Markdown, Templates, Security,
 			Cross-Platform, Digital Notebook, Knowledge Management
 		</p>
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">technology</h2>
-		<p class="text-gray-700">
+		<h2 class="text-2xl font-bold text-gray-100 dark:text-gray-200">technology</h2>
+		<p class="text-gray-300 dark:text-gray-700">
 			Utilize a digital notebook that scales with your needs, offering a smooth user experience and
 			future-proof features. With minimalistic design and powerful functionality, Liber Notarum
 			ensures your notes are always accessible, secure, and well-organized.
@@ -74,8 +74,8 @@
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">style</h2>
-		<p class="text-gray-700">
+		<h2 class="text-2xl font-bold text-gray-100 dark:text-gray-200">style</h2>
+		<p class="text-gray-300 dark:text-gray-700">
 			Enjoy a clean, minimalist design with customizable colors and layouts. Combine and modify
 			elements to create a personalized note-taking experience that balances simplicity and
 			sophistication.
@@ -83,8 +83,8 @@
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">open-source license</h2>
-		<p class="text-gray-700">
+		<h2 class="text-2xl font-bold text-gray-100 dark:text-gray-200">open-source license</h2>
+		<p class="text-gray-300 dark:text-gray-700">
 			Liber Notarum is completely free for personal and commercial use, with no attribution
 			required. Embrace the freedom to use, modify, and share without restrictions. Just don’t
 			replicate or redistribute the platform as your own.
@@ -92,22 +92,22 @@
 	</section>
 
 	<section class="my-6">
-		<h2 class="text-2xl font-bold text-gray-800">history</h2>
-		<p class="text-gray-700">
+		<h2 class="text-2xl font-bold text-gray-100 dark:text-gray-200">history</h2>
+		<p class="text-gray-300 dark:text-gray-700">
 			Launched in 2024, Liber Notarum began with the goal of enhancing digital note-taking. Inspired
 			by the open-source community, it has quickly grown to be a trusted tool for individuals and
 			organizations alike, providing an exceptional note-taking experience across various industries
 			and use cases.
 		</p>
-		<a
+		<!-- <a
 			href="/signup"
-			class="mt-4 inline-block rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+			class="mt-4 inline-block rounded bg-red-600 px-4 py-2 text-white hover:bg-red-700 dark:bg-red-400 dark:hover:bg-red-500"
 			>Sign up now</a
-		>
+		> -->
 	</section>
 </main>
 
-<footer class="bg-gray-800 p-6 text-center text-white">
+<footer class="bg-secondary-light p-6 text-center text-white">
 	<!-- <div>
 		<a href="https://twitter.com/libernotarum" class="mx-2 inline-block">Twitter</a>
 		<a href="https://facebook.com/libernotarum" class="mx-2 inline-block">Facebook</a>
